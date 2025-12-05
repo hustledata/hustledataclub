20f7c60983a8aca7b8412ee72bf2767de9e47b1b
Hustle Data <ihustledata@outlook.com>
2025-12-05 13:28:42 -0500

    Build Site

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/',
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/',          // correct for a custom domain like hustledata.club
+  build: {
+    outDir: 'docs',   // ⬅ Vite will put the built site into /docs
+  },
 })
+
